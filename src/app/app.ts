96fced5240310b551552e5aa1d1be19a90f11583
Carlos Garita Campos <cgaritac@gmail.com>
2026-02-13 22:29:05 -0600

    Feat: Chanes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './Layout/navbar/navbar.component';
 import { FooterComponent } from './Layout/footer/footer.component';
+import { HeaderComponent } from "./Layout/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
