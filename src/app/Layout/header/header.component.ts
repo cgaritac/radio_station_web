7c99b57fcf8af24258578d94a4daf6e6aada8226
Carlos Garita Campos <cgaritac@gmail.com>
2026-02-14 23:42:42 -0600

    Feat: Configure footer

--- src/app/Layout/header/header.component.ts
+++ src/app/Layout/header/header.component.ts
@@ -1,14 +1,17 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SvgIconComponent } from "angular-svg-icon";
+import { RadioService } from '../../Core/services/radio.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavbarComponent, SvgIconComponent],
+  imports: [NavbarComponent, SvgIconComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent {
+  radioService = inject(RadioService);
 }
