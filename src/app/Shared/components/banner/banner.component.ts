c9dc03e7e338050d9339378c89f631063af2b8f9
Carlos Garita Campos <cgaritac@gmail.com>
2026-02-19 01:15:13 -0600

    Feat: Add banner

---
+++ src/app/Shared/components/banner/banner.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActionButtonComponent } from '../action-button/action-button.component';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-banner',
+  standalone: true,
+  imports: [CommonModule, ActionButtonComponent, SvgIconComponent, TranslateModule],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BannerComponent {
+  @Input() title: string = '';
+  @Input() description: string = '';
+
+  // Styles
+  @Input() backgroundColor: string = 'bg-brand-quaternary';
+  @Input() textColor: string = 'text-brand-primary';
+  @Input() iconColor: string = 'text-brand-primary';
+  @Input() buttonColor: string = 'bg-brand-primary';
+  @Input() buttonTextColor: string = 'text-brand-primary';
+  @Input() width: string = 'w-full';
+  @Input() height: string = 'h-full';
+  
+  // Primary button
+  @Input() primaryText: string = '';
+  @Input() primaryLink: string = '';
+  @Input() primaryIcon: string = '';
+
+  // Secondary button
+  @Input() secondaryText: string = '';
+  @Input() secondaryLink: string = '';
+  @Input() secondaryIcon: string = '';
+
+  // Decoration
+  @Input() iconName: string = 'microphone';
+}
