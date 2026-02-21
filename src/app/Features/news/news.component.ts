b7d2d5cce2c554a97b2f23225ed674db6012539e
Carlos Garita Campos <cgaritac@gmail.com>
2026-02-20 22:44:29 -0600

    Feat: Add community section

--- src/app/Features/news/news.component.ts
+++ src/app/Features/news/news.component.ts
@@ -1,12 +1,35 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { ActionButtonComponent } from '../../Shared/components/action-button/action-button.component';
 
 @Component({
   selector: 'app-news',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, SvgIconComponent, ActionButtonComponent],
   templateUrl: './news.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NewsComponent {}
+export class NewsComponent {
+  activities = [
+    {
+      title: 'Culto Unido de Oración',
+      date: '15 DE OCTUBRE, 2026',
+      location: 'Plaza Central, Nandayure',
+      image: 'images/news-people.jpg'
+    },
+    {
+      title: 'Reunión de Jóvenes: "Fe Activa"',
+      date: '22 DE OCTUBRE, 2026',
+      location: 'Auditorio Municipal',
+      image: 'images/news-microphone.jpg'
+    },
+    {
+      title: 'Campaña de Ayuda Social',
+      date: '02 DE NOVIEMBRE, 2026',
+      location: 'Barrio La Esperanza',
+      image: 'images/news-helping.jpg'
+    }
+  ];
+}
