5bd496de1bb395fae96c2f5fde5b57ced54107b4
Carlos Garita Campos <cgaritac@gmail.com>
2026-02-24 23:26:42 -0600

    Feat: Contact page refactor

---
+++ src/app/Shared/components/contact-info/contact-info.component.ts
@@ -0,0 +1,17 @@
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { SectionHeaderComponent } from '../section-header/section-header.component';
+import { RadioService } from '../../../Core/services/radio.service';
+
+@Component({
+  selector: 'app-contact-info',
+  standalone: true,
+  imports: [CommonModule, TranslateModule, SvgIconComponent, SectionHeaderComponent],
+  templateUrl: './contact-info.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContactInfoComponent {
+  protected readonly radioService = inject(RadioService);
+}
---
+++ src/app/Shared/components/location-map/location-map.component.ts
@@ -0,0 +1,48 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  signal,
+  inject,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
+import { RadioService } from '../../../Core/services/radio.service';
+
+@Component({
+  selector: 'app-location-map',
+  standalone: true,
+  imports: [CommonModule, TranslateModule],
+  templateUrl: './location-map.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LocationMapComponent implements OnInit, OnDestroy {
+  private readonly sanitizer = inject(DomSanitizer);
+  private readonly translateService = inject(TranslateService);
+  private readonly radioService = inject(RadioService);
+
+  private langSub?: Subscription;
+
+  readonly mapUrl = signal<SafeResourceUrl>(this.sanitizer.bypassSecurityTrustResourceUrl(''));
+
+  ngOnInit() {
+    this.updateMapUrl();
+    this.langSub = this.translateService.onLangChange.subscribe(() => {
+      this.updateMapUrl();
+    });
+  }
+
+  ngOnDestroy() {
+    this.langSub?.unsubscribe();
+  }
+
+  private updateMapUrl() {
+    const address = this.translateService.instant('FOOTER.ADDRESS');
+    const baseUrl = this.radioService.socialLinks.googleMapsEmbed;
+    const url = `${baseUrl}${encodeURIComponent(address)}&t=&z=15&ie=UTF8&iwloc=&output=embed`;
+    this.mapUrl.set(this.sanitizer.bypassSecurityTrustResourceUrl(url));
+  }
+}
